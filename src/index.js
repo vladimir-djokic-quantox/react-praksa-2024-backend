b86ec4800c1765b62025038957850f9da936be37
Vladimir Djokic <vladimir.djokic@quantox.com>
2024-02-06 18:18:16 +0100

    Add `clearCart: CartEntityResponse!` mutation.

--- src/index.js
+++ src/index.js
@@ -8,17 +8,14 @@ module.exports = {
    * This gives you an opportunity to extend code.
    */
   register({ strapi }) {
-    /**
-     * 1) Add `restaurant(slug: String!)` query.
-     * 2) Extend `me` query with `cart` and `orders`.
-     */
     const { toEntityResponse, toEntityResponseCollection } = strapi
       .plugin("graphql")
       .service("format").returnTypes;
 
     const extensionService = strapi.plugin("graphql").service("extension");
 
-    extensionService.use(({ nexus }) => ({
+    /** Add `restaurant(slug: String!)` query. */
+    extensionService.use(() => ({
       typeDefs: `
         type Query {
           restaurantBySlug(slug: String!): RestaurantEntityResponse
@@ -28,17 +25,82 @@ module.exports = {
         Query: {
           restaurantBySlug: {
             resolve: async (parent, args, context) => {
-              const data = await strapi.services[
-                "api::restaurant.restaurant"
-              ].find({
-                filters: { slug: args.slug },
-              });
+              const restaurants = await strapi.entityService.findMany(
+                "api::restaurant.restaurant",
+                {
+                  filters: { slug: args.slug },
+                }
+              );
 
-              return toEntityResponse(data.results[0]);
+              return toEntityResponse(restaurants[0]);
             },
           },
         },
       },
+    }));
+
+    /** Add `addToCart(dishes: [ID]!)` mutation. */
+    // ...
+
+    /** Add `removeFromCart(dish: ID!)` mutation. */
+    // ...
+
+    /** Add `clearCart()` mutation. */
+    extensionService.use(({ strapi }) => ({
+      typeDefs: `
+        type Mutation {
+          clearCart: CartEntityResponse!
+        }
+      `,
+      resolvers: {
+        Mutation: {
+          clearCart: {
+            resolve: async (parent, args, context) => {
+              const userId = context.state.user.id;
+
+              const carts = await strapi.entityService.findMany(
+                "api::cart.cart",
+                {
+                  filters: {
+                    user: {
+                      id: {
+                        eq: userId,
+                      },
+                    },
+                  },
+                }
+              );
+
+              const shouldUpdate = !!carts.length;
+
+              const cart = shouldUpdate
+                ? carts[0]
+                : await strapi.entityService.create("api::cart.cart", {
+                    data: {
+                      user: userId,
+                      dishes: [],
+                    },
+                  });
+
+              if (shouldUpdate) {
+                /** Clear dishes from the cart. */
+                await strapi.entityService.update("api::cart.cart", cart.id, {
+                  data: {
+                    user: userId,
+                    dishes: [],
+                  },
+                });
+              }
+
+              return toEntityResponse(cart);
+            },
+          },
+        },
+      },
+    }));
+
+    /** Extend `me` query with `cart` and `orders`. */
+    extensionService.use(({ nexus }) => ({
       types: [
         nexus.extendType({
           type: "UsersPermissionsMe",
